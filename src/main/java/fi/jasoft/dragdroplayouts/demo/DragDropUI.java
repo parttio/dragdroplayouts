97adb00fcb8fa28b59c57dad30e4a082bf139e40
John Ahlroos <john.ahlroos@jasoft.fi>
2012-12-09 15:26:09 +0200

    Implemented new demo application

--- src/main/java/fi/jasoft/dragdroplayouts/demo/DragDropUI.java
+++ src/main/java/fi/jasoft/dragdroplayouts/demo/DragDropUI.java
@@ -8,81 +8,112 @@ import java.util.regex.Pattern;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.Item;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Panel;
-import com.vaadin.ui.TabSheet;
-import com.vaadin.ui.TabSheet.SelectedTabChangeEvent;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.VerticalSplitPanel;
+import com.vaadin.ui.themes.Reindeer;
 
 @Theme("dragdrop")
 @Widgetset("fi.jasoft.dragdroplayouts.demo.DemoWidgetSet")
 public class DragDropUI extends UI {
 
-    private TabSheet tabs;
+    private final Label code = new Label("No source code available.");
 
-    private Label code;
+    private final ListSelect componentList = new ListSelect();
+
+    private final VerticalSplitPanel split = new VerticalSplitPanel();
+
+    public DragDropUI() {
+        componentList.setNullSelectionAllowed(false);
+        componentList.addContainerProperty("caption", String.class, "");
+        componentList.setWidth("200px");
+        componentList.setHeight("100%");
+        componentList.setImmediate(true);
+        componentList
+                .addValueChangeListener(new Property.ValueChangeListener() {
+
+                    @Override
+                    public void valueChange(ValueChangeEvent event) {
+                        try {
+                            tabChanged((Component) event.getProperty()
+                                    .getValue());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+        componentList.setItemCaptionPropertyId("caption");
+        componentList.setItemCaptionMode(ItemCaptionMode.PROPERTY);
+
+        addDemo(new DragdropAbsoluteLayoutDemo());
+        addDemo(new DragdropVerticalLayoutDemo());
+        addDemo(new DragdropHorizontalLayoutDemo());
+        addDemo(new DragdropGridLayoutDemo());
+        addDemo(new DragdropCssLayoutDemo());
+        addDemo(new DragdropFormLayoutDemo());
+
+        addDemo(new DragdropLayoutDraggingDemo());
+        addDemo(new DragdropHorizontalSplitPanelDemo());
+        addDemo(new DragdropVerticalSplitPanelDemo());
+        addDemo(new DragdropTabsheetDemo());
+        addDemo(new DragdropAccordionDemo());
+
+        addDemo(new DragdropDragFilterDemo());
+        addDemo(new DragdropCaptionModeDemo());
+    }
+
+    private void addDemo(CustomComponent view) {
+        Item item = componentList.addItem(view);
+        item.getItemProperty("caption").setValue(view.getCaption());
+    }
 
     @Override
     protected void init(VaadinRequest request) {
+        VerticalLayout root = new VerticalLayout();
+        root.setSizeFull();
+        root.setSpacing(true);
+        root.setMargin(true);
+        setContent(root);
 
-        VerticalSplitPanel content = new VerticalSplitPanel();
-        content.setSizeFull();
+        Label header = new Label("DragDropLayouts for Vaadin 7");
+        header.setStyleName(Reindeer.LABEL_H1);
+        root.addComponent(header);
 
-        content.addComponent(new DragdropAbsoluteLayoutDemo());
+        Panel subroot = new Panel(new HorizontalLayout());
+        ((HorizontalLayout) subroot.getContent()).setSizeFull();
+        subroot.setSizeFull();
 
-        setContent(content);
+        ((HorizontalLayout) subroot.getContent()).addComponent(componentList);
+        root.addComponent(subroot);
+        root.setExpandRatio(subroot, 1);
 
-        tabs = new TabSheet();
-        tabs.setSizeFull();
-        tabs.setImmediate(true);
-
-        tabs.addComponent(new DragdropAbsoluteLayoutDemo());
-        tabs.addComponent(new DragdropVerticalLayoutDemo());
-        tabs.addComponent(new DragdropHorizontalLayoutDemo());
-        tabs.addComponent(new DragdropGridLayoutDemo());
-        tabs.addComponent(new DragdropCssLayoutDemo());
-        tabs.addComponent(new DragdropFormLayoutDemo());
-
-        tabs.addComponent(new DragdropLayoutDraggingDemo());
-        tabs.addComponent(new DragdropHorizontalSplitPanelDemo());
-        tabs.addComponent(new DragdropVerticalSplitPanelDemo());
-        tabs.addComponent(new DragdropTabsheetDemo());
-        tabs.addComponent(new DragdropAccordionDemo());
-
-        tabs.addComponent(new DragdropDragFilterDemo());
-        tabs.addComponent(new DragdropCaptionModeDemo());
-
-        tabs.addListener(new TabSheet.SelectedTabChangeListener() {
-            public void selectedTabChange(SelectedTabChangeEvent event) {
-                try {
-                    tabChanged(event.getTabSheet().getSelectedTab());
-                } catch (IOException e) {
-                    code.setValue("No source code available.");
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        content.setFirstComponent(tabs);
-
-        code = new Label("");
-        try {
-            tabChanged(tabs.getSelectedTab());
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+        split.setSizeFull();
+        split.setSplitPosition(30, Unit.PERCENTAGE, true);
+        ((HorizontalLayout) subroot.getContent()).addComponent(split);
+        ((HorizontalLayout) subroot.getContent()).setExpandRatio(split, 1);
 
         Panel codePanel = new Panel();
         codePanel.setSizeFull();
         codePanel.setContent(code);
-        content.setSecondComponent(codePanel);
+        split.setSecondComponent(codePanel);
+
+        componentList.select(componentList.getItemIds().iterator().next());
     }
 
     private void tabChanged(Component tab) throws IOException {
+        split.setFirstComponent(tab);
+
         String path = tab.getClass().getCanonicalName().replaceAll("\\.", "/")
                 + ".java";
 
