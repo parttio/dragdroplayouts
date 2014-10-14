bbc4b1502e75cf6c5acaa958c1e4ab065ea52ea9
John Ahlroos <john.ahlroos@jasoft.fi>
2014-10-14 21:17:21 +0300

    Javadoc

--- addon/src/main/java/fi/jasoft/dragdroplayouts/client/ui/util/HTML5Support.java
+++ addon/src/main/java/fi/jasoft/dragdroplayouts/client/ui/util/HTML5Support.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2014 John Ahlroos
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package fi.jasoft.dragdroplayouts.client.ui.util;
 
 import java.util.ArrayList;
@@ -23,6 +36,11 @@ import com.vaadin.client.ui.dd.VTransferable;
 
 import fi.jasoft.dragdroplayouts.client.ui.VDDAbstractDropHandler;
 
+/**
+ * Provides HTML5 drops for any connector
+ * 
+ * @author John Ahlroos / www.jasoft.fi
+ */
 public class HTML5Support {
 
   private final List<HandlerRegistration> handlers = new ArrayList<HandlerRegistration>();
